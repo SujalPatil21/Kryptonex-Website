b77fecd65393c9235d8d00fbdab1b4d48e4ec722
yash-yb <yashbhanushali452007@gmail.com>
2026-04-03 22:46:28 +0530

    Added images of team members for PR and documentation

--- src/data/teamData.ts
+++ src/data/teamData.ts
@@ -1,5 +1,13 @@
 import avaneesh from '../assets/team/avaneesh_yajurvedi.jpg'
 import durgesh from '../assets/team/Durgesh_President.jpg'
+import atharv from '../assets/team/atharv_madan_barge.jpg'
+import yashika from '../assets/team/yashika_dhumal.jpg'
+import khushi from '../assets/team/khushi_gupta.jpg'
+import sanskar from '../assets/team/sanskar_yuvraj_virat.jpg'
+import samruddhi from '../assets/team/samruddhi_bhosale.png'
+import shrutika from '../assets/team/shrutika_bhand.jpg'
+import arya from '../assets/team/arya_jain.jpeg'
+import anuj from '../assets/team/anuj_deore.png'
 
 export interface TeamMember {
   name: string
@@ -63,18 +71,55 @@ const teamData: TeamData = {
     { name: '', role: 'Team Member' },
   ],
 
-  contentPR: [
-    { name: '', role: 'Team Head' },
-    { name: '', role: 'Team Member' },
-    { name: '', role: 'Team Member' },
+    contentPR: [
+    { name: 'Atharv Madan Barge', 
+      role: 'Team Head' , 
+      image: atharv,
+      github: 'https://github.com/atharvbarge-bytes',
+      linkedin: 'https://www.linkedin.com/in/atharv-barge-b32a44362/',
+    },
+    { name: 'Yashika A. Dhumal', 
+      role: 'Team Member', 
+      image : yashika
+    },
+    { name: 'Khushi Rajesh Gupta ', 
+      role: 'Team Member', 
+      image: khushi,
+      linkedin: 'https://www.linkedin.com/in/atharv-barge-b32a44362/',
+    },
     { name: '', role: 'Team Member' },
   ],
 
   documentation: [
-    { name: '', role: 'Team Head' },
-    { name: '', role: 'Team Member' },
-    { name: '', role: 'Team Member' },
-    { name: '', role: 'Team Member' },
+    { name: 'Sanskar Yuvraj Virat', 
+      role: 'Team Head', 
+      image: sanskar,
+      github: 'https://github.com/sanskar-sol',
+      linkedin: 'https://www.linkedin.com/in/sanskar-virat-95b533390/'
+    },
+    { name: 'Samruddhi Bhosale', 
+      role: 'Team Head', 
+      image: samruddhi,
+      github: 'https://github.com/samruddhibhosale17',
+      linkedin: 'https://www.linkedin.com/in/samruddhi-bhosale-4863ab377/'
+    },
+    { name: 'Shrutika Bhand', 
+      role: 'Team Member',
+      image: shrutika,
+      github: 'https://github.com/shrutikabhand',
+      linkedin: 'https://www.linkedin.com/in/shrutika-bhand-139267370/'
+    },
+    { name: 'Aarya Jain ', 
+      role: 'Team Member', 
+      image: arya,
+      github: 'https://github.com/aaryaa-jain',
+      linkedin: 'https://www.linkedin.com/in/aarya-jain-60ba753a5/'
+    },
+    { name: 'Anuj Deore', 
+      role: 'Team Member',
+      image: anuj,
+      linkedin: 'https://www.linkedin.com/in/anuj-deore-70b3003a1'
+    },
   ],
 }
 
